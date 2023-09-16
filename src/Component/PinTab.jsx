713885fc85ec2c4d3824edfd73b3599f6dbd50c2
Golu <golubook09@gmail.com>
2023-09-16 15:36:33 +0100

    create auto inpute changer 1 to n

--- src/Component/PinTab.jsx
+++ src/Component/PinTab.jsx
@@ -1,11 +1,25 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 export const PinTab = ({length, maxchar}) => {
     const [pinLength, setPinlength] = useState(new Array(length).fill(""))
-  return (
-    <>{
+    let inputref= useRef([]);
+    console.log(inputref)
+    const handelFocus= (e, index)=>{
+        if(e.target.value.length===maxchar && index<length-1){
+          inputref.current[index+1].focus();
+        }
+    }
+    return (
+    <>
+    {
     pinLength.map((_, index)=>{
-        return <input key ={index} maxLength={maxchar}></input>
+        return <input key ={index}
+        ref={(e)=>{
+          inputref.current[index]=e;
+        }} 
+        maxLength={maxchar}
+        onChange={(e)=>{handelFocus(e,index)}}
+        ></input>
     })
 }
     </>
